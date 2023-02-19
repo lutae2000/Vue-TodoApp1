2ea59a40bca08049af359847c8f1b4baa549cc6d
utlee <lutae2000@gmail.com>
2023-02-20 01:26:25 +0900

    TODO app first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        mode: "list",
+        memo: {
+            id: null,
+            content: null,
+            regDate: null,
+        },
+        memos: [],
+    },
+    methods: {
+        renew: function (val) {
+            return JSON.parse(JSON.stringify(val));
+        },
+        open: function (id) {
+            for (var i in this.memos) {
+                if (this.memos[i].id === id) {
+                    this.memo = this.renew(this.memos[i]);
+                    break;
+                }
+            }
+            this.mode = "edit";
+        },
+        write: function () {
+            this.mode = "write";
+            this.memo = {
+                id: null,
+                content: null,
+                regDate: null,
+            };
+        },
+        save: function () {
+            this.memos.push({
+                id: this.memos.length + 1,
+                content: this.memo.content,
+                regDate: new Date(),
+            });
+            this.mode = "list";
+
+            localStorage.setItem("memos", JSON.stringify(this.memos));
+        },
+        cancel: function () {
+            this.mode = "list";
+        },
+    },
+    created: function () {
+        let memos = localStorage.getItem("memos");
+        if (memos) {
+            this.memos = JSON.parse(memos);
+        }
+    },
+});
